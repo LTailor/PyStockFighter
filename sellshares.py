56817f897cba8285e0e0e9e7ecb2903b75d78c3b
Dzhambulat Khasayev <dzkhasayev@gmail.com>
2015-12-23 00:10:04 +0300

    basic functions

--- sellshares.py
+++ sellshares.py
@@ -1,8 +1,47 @@
 __author__ = 'Dzhambulat'
 
 from stocklib.stockfighter import *
+import math
+import time
 
 setApiKey('')
-makeBuyLimitOrder('WCC56232173','YIWO','ASDEX',100,22)
 
+share_num=100000
+current_num=0
 
+account=''
+
+symbol=''
+venue=''
+price_avg=0.0
+prices=[]
+info_num=30
+
+for i in range(info_num):
+    r=getStockInfo(symbol,venue)
+    price_avg+=r['last']
+    prices.append(r['last'])
+    print (r['last'])
+    time.sleep(3)
+price_avg /= info_num
+
+sqr_differs=[(p-price_avg)*(p-price_avg) for p in prices]
+std=sum(sqr_differs)/25.0
+std=math.sqrt(std)
+
+print "Average price is "+ str(price_avg)
+print "Std is "+str(std)
+
+if(std>0):
+    bid_price=price_avg/100.0-1.96*std/100.0+0.2
+    ask_price=price_avg/100.0+1.96*std/100.0-0.2
+else:
+    bid_price=price_avg-0.2
+    ask_price=price_avg+0.2
+
+print "Bid price: "+str(bid_price)
+print "Ask price: "+str(ask_price)
+
+makeBuyLimitOrder(account,symbol,venue,10,bid_price)
+
+#makeSellLimitOrder(account,symbol,venue,10,ask_price)
--- stocklib/stockfighter.py
+++ stocklib/stockfighter.py
@@ -16,7 +16,7 @@ def makeBuyLimitOrder(account,symbol, venue, qty, price):
         "qty":qty,
         "venue":venue,
         "symbol":symbol,
-        "price":price*100,
+        "price":int(price)*100,
         "direction":"buy",
         "orderType":"limit"
     }
@@ -24,5 +24,56 @@ def makeBuyLimitOrder(account,symbol, venue, qty, price):
     headers={
         "X-Starfighter-Authorization" : apikey
     }
-    req = requests.post("https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders".format(venue,symbol),data=json.dumps(order),headers=headers)
-    print req.text
+    resp = requests.post("https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders".format(venue,symbol),data=json.dumps(order),headers=headers)
+    print resp.text
+    return json.loads(resp.text)
+
+def makeSellLimitOrder(account,symbol,venue,qty,price):
+
+    order={
+        "account":account,
+        "qty":qty,
+        "venue":venue,
+        "symbol":symbol,
+        "price":int(price)*100,
+        "direction":"sell",
+        "orderType":"limit"
+    }
+
+    headers={
+        "X-Starfighter-Authorization" : apikey
+    }
+    resp = requests.post("https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders".format(venue,symbol),data=json.dumps(order),headers=headers)
+    print resp.text
+    return json.loads(resp.text)
+
+def getOrderStatus(orderid,symbol,venue):
+
+    headers={
+        "X-Starfighter-Authorization" : apikey
+    }
+
+    url="https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders/{2}".format(venue,symbol,orderid)
+
+    resp = requests.post(url,headers=headers)
+
+    return json.loads(resp.text)
+
+def cancelOrder(orderid,symbol,venue):
+
+    headers={
+        "X-Starfighter-Authorization" : apikey
+    }
+
+    url="https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders/{2}".format(venue,symbol,orderid)
+
+    resp = requests.post(url,headers=headers)
+
+    return json.loads(resp.text)
+
+def getStockInfo(symbol,venue):
+
+    url="https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/quote".format(venue,symbol)
+
+    resp = requests.get(url)
+    return json.loads(resp.text)
